4604ccc877b3449047a83bec7a088854963ea713
Justin Barhite <jbarhite@carthage.edu>
2015-02-24 15:23:50 -0600

    Added file to test MOSFET.

---
+++ Test_MOSFET/Test_MOSFET.ino
@@ -0,0 +1,18 @@
+#define PWM 3
+
+void setup() {
+
+}
+
+void loop() {
+	for (int i=0; i<=255; i++) {
+		analogWrite(PWM, i);
+		delay(10);
+	}
+	delay(2000);
+	for (int i=255; i>=255; i--) {
+		analogWrite(PWM, i);
+		delay(10);
+	}
+	delay(2000);
+}
